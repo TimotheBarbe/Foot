5bfe52e02e285c1fa899780347aa3c1385aada00
florenteuvrard <flo.euv@gmail.com>
2014-12-01 16:27:40 +0100

    extends AbstractProblem

--- Foot/src/solveur/Recherche.java
+++ Foot/src/solveur/Recherche.java
@@ -1,7 +1,7 @@
 package solveur;
 
 import model.Desiderata;
-import solver.ResolutionPolicy;
+import samples.AbstractProblem;
 import solver.Solver;
 import solver.constraints.IntConstraintFactory;
 import solver.constraints.LogicalConstraintFactory;
@@ -14,7 +14,7 @@ import solver.trace.Chatterbox;
 import solver.variables.IntVar;
 import solver.variables.VariableFactory;
 
-public class Recherche {
+public class Recherche extends AbstractProblem {
 
 	private int nbClub;
 	private int nbGroupe;
@@ -24,10 +24,12 @@ public class Recherche {
 	private int[] distance;
 	private Desiderata[] listeDesiderata;
 	private long tempsMax;
-	private int distMax = 450;
+	private int distMax = 600;
+	private IntVar[] club;
+	private IntVar sommeDist;
+	private IntVar[] tabDist;
 
-	public Recherche(int nbGroupe, int[][] tabDistance,
-			Desiderata[] listeDesiderata, long tempsMax) {
+	public Recherche(int nbGroupe, int[][] tabDistance, Desiderata[] listeDesiderata, long tempsMax) {
 		this.nbGroupe = nbGroupe;
 		this.tabDistance = tabDistance;
 		this.nbClub = this.tabDistance.length;
@@ -38,72 +40,101 @@ public class Recherche {
 		this.tempsMax = tempsMax;
 	}
 
-	public void lancer() {
-		Solver s = new Solver();
+	public String getSolution() {
+		String rep = "Solution:  ";
+		for (int i = 0; i < solution.length; i++) {
+			rep += "   " + solution[i];
+		}
+		return rep;
+	}
+
+	public String getDistance() {
+		String rep = "Distances: ";
+		for (int i = 0; i < distance.length; i++) {
+			rep += "  " + distance[i];
+		}
+		return rep;
+	}
+
+	@Override
+	public void buildModel() {
 
 		// solution au pb : si club[2]=3, le 2eme club est dans la poule 3
 
-		IntVar[] club = new IntVar[nbClub];
+		club = new IntVar[nbClub];
 
 		for (int i = 0; i < nbClub; i++) {
-			club[i] = VariableFactory.bounded("Club " + i + " ", 0,
-					nbGroupe - 1, s);
+			club[i] = VariableFactory.bounded("Club " + i + " ", 0, nbGroupe - 1, solver);
 		}
 
 		// Pas plus de clubs que la cap max d'une poule
 
 		for (int i = 0; i < nbGroupe; i++) {
-			IntVar e = VariableFactory.bounded("E " + i, tailleGroupe - 1,
-					tailleGroupe, s);
-			s.post(IntConstraintFactory.count(i, club, e));
+			IntVar e = VariableFactory.bounded("E " + i, tailleGroupe - 1, tailleGroupe, solver);
+			solver.post(IntConstraintFactory.count(i, club, e));
 		}
 
 		// Somme des distances pour chaque poule
 
-		IntVar zero = VariableFactory.fixed(0, s);
-		IntVar un = VariableFactory.fixed(1, s);
+		IntVar zero = VariableFactory.fixed(0, solver);
+		IntVar un = VariableFactory.fixed(1, solver);
 
-		IntVar[] tabDist = new IntVar[nbClub];
+		tabDist = new IntVar[nbClub];
 		for (int j = 0; j < nbClub; j++) {
-			IntVar[] d = VariableFactory
-					.boolArray("memeGroupe " + j, nbClub, s);
+			IntVar[] d = VariableFactory.boolArray("memeGroupe " + j, nbClub, solver);
 			for (int i = 0; i < d.length; i++) {
-				LogicalConstraintFactory.ifThenElse(
-						IntConstraintFactory.arithm(club[j], "=", club[i]),
-						IntConstraintFactory.arithm(d[i], "=", un),
-						IntConstraintFactory.arithm(d[i], "=", zero));
+				LogicalConstraintFactory.ifThenElse(IntConstraintFactory.arithm(club[j], "=", club[i]),
+						IntConstraintFactory.arithm(d[i], "=", un), IntConstraintFactory.arithm(d[i], "=", zero));
 			}
-			IntVar distance = VariableFactory.bounded("distance" + j, 0, 10000,
-					s);
-			s.post(IntConstraintFactory.scalar(d, tabDistance[j], distance));
+			IntVar distance = VariableFactory.bounded("distance" + j, 0, 10000, solver);
+			solver.post(IntConstraintFactory.scalar(d, tabDistance[j], distance));
 			tabDist[j] = distance;
 		}
 
-		IntVar sommeDist = VariableFactory.bounded("somme distance", 0, 100000,
-				s);
-		s.post(IntConstraintFactory.sum(tabDist, sommeDist));
-		s.post(IntConstraintFactory.arithm(sommeDist, "<", this.distMax));
+		sommeDist = VariableFactory.bounded("somme distance", 0, 100000, solver);
+		solver.post(IntConstraintFactory.sum(tabDist, sommeDist));
+		solver.post(IntConstraintFactory.arithm(sommeDist, "<", this.distMax));
 		// Desiderata
 		for (Desiderata d : listeDesiderata) {
-			s.post(IntConstraintFactory.arithm(club[d.getClub1()], d.getOp(),
-					club[d.getClub2()]));
+			solver.post(IntConstraintFactory.arithm(club[d.getClub1()], d.getOp(), club[d.getClub2()]));
 		}
 
+	}
+
+	@Override
+	public void configureSearch() {
+
 		// Limitation en temps du probleme
-		SearchMonitorFactory.limitTime(s, this.tempsMax);
+		SearchMonitorFactory.limitTime(solver, this.tempsMax);
 
-		AbstractStrategy activity = IntStrategyFactory.activity(club,15012011);
-		s.set(IntStrategyFactory.lastConflict(s,activity));
-		SMF.geometrical(s,(nbClub)/2,2,new FailCounter(nbClub),Integer.MAX_VALUE);
-		
-		// Resolution
-		
-		Chatterbox.showStatisticsDuringResolution(s, 1000);
-		//s.findOptimalSolution(ResolutionPolicy.MINIMIZE, sommeDist);
-		s.findSolution();
+		AbstractStrategy activity = IntStrategyFactory.activity(club, 15012011);
+		solver.set(IntStrategyFactory.lastConflict(solver, activity));
+		SMF.geometrical(solver, (nbClub) / 2, 2, new FailCounter(nbClub), Integer.MAX_VALUE);
 
-		System.out.println(sommeDist.getValue());
-		
+		Chatterbox.showStatisticsDuringResolution(solver, 5000);
+
+	}
+
+	@Override
+	public void createSolver() {
+		solver = new Solver();
+
+	}
+
+	@Override
+	public void prettyOut() {
+		System.out.println("Somme Distance : " + sommeDist.getValue());
+
+		System.out.println(getSolution());
+		System.out.println(getDistance());
+
+	}
+
+	@Override
+	public void solve() {
+		// s.findOptimalSolution(ResolutionPolicy.MINIMIZE, sommeDist);
+		solver.findSolution();
+
 		// Memorisation
 		for (int i = 0; i < this.nbClub; i++) {
 			this.solution[i] = club[i].getValue();
@@ -112,21 +143,5 @@ public class Recherche {
 		for (int i = 0; i < this.nbClub; i++) {
 			this.distance[i] = tabDist[i].getValue();
 		}
-	}
-
-	public String getSolution() {
-		String rep = "Solution: ";
-		for (int i = 0; i < solution.length; i++) {
-			rep += "  " + solution[i];
-		}
-		return rep;
-	}
-
-	public String getDistance() {
-		String rep = "Distances : ";
-		for (int i = 0; i < distance.length; i++) {
-			rep += "  " + distance[i];
-		}
-		return rep;
 	}
 }
--- Foot/src/tests/TestRecherche.java
+++ Foot/src/tests/TestRecherche.java
@@ -7,34 +7,25 @@ import solveur.Recherche;
 public class TestRecherche {
 
 	public static void main(String[] args) {
-		int[][] tab = new int[][] { { 0, 20, 1, 5 }, { 20, 0, 10, 3 },
-				{ 1, 10, 0, 1 }, { 5, 3, 1, 0 } };
+		int[][] tab = new int[][] { { 0, 20, 1, 5 }, { 20, 0, 10, 3 }, { 1, 10, 0, 1 }, { 5, 3, 1, 0 } };
 
-		int[][] tab_moyen = new int[][] { { 0, 10, 5, 3, 5, 3, 20, 6, 9, 22 },
-				{ 10, 0, 6, 3, 1, 20, 6, 8, 9, 30 },
-				{ 5, 6, 0, 7, 10, 20, 1, 5, 8, 9 },
-				{ 3, 3, 7, 0, 12, 2, 2, 5, 12, 10 },
-				{ 5, 1, 10, 12, 0, 3, 8, 4, 2, 1 },
-				{ 3, 20, 20, 2, 3, 0, 3, 2, 6, 10 },
-				{ 20, 6, 1, 2, 8, 2, 0, 10, 1, 1 },
-				{ 6, 8, 5, 5, 4, 2, 10, 0, 3, 12 },
-				{ 9, 9, 8, 12, 2, 6, 1, 3, 0, 5 },
-				{ 22, 30, 9, 10, 1, 10, 1, 12, 5, 0 } };
+		int[][] tab_moyen = new int[][] { { 0, 10, 5, 3, 5, 3, 20, 6, 9, 22 }, { 10, 0, 6, 3, 1, 20, 6, 8, 9, 30 },
+				{ 5, 6, 0, 7, 10, 20, 1, 5, 8, 9 }, { 3, 3, 7, 0, 12, 2, 2, 5, 12, 10 },
+				{ 5, 1, 10, 12, 0, 3, 8, 4, 2, 1 }, { 3, 20, 20, 2, 3, 0, 3, 2, 6, 10 },
+				{ 20, 6, 1, 2, 8, 2, 0, 10, 1, 1 }, { 6, 8, 5, 5, 4, 2, 10, 0, 3, 12 },
+				{ 9, 9, 8, 12, 2, 6, 1, 3, 0, 5 }, { 22, 30, 9, 10, 1, 10, 1, 12, 5, 0 } };
 
 		Desiderata d1 = new Desiderata(0, 1, "=");
-		Desiderata[] tabDesiderata = new Desiderata[] {  };
+		Desiderata[] tabDesiderata = new Desiderata[] {};
 
-		int[][] tab_enorme = GenerateDistances.genererTableau(15, 20);
+		int[][] tab_enorme = GenerateDistances.genererTableau(17, 20);
 
 		long millis = System.currentTimeMillis();
-		
+
 		int nbg = 3;
-		Recherche r = new Recherche(nbg, tab_enorme, tabDesiderata, 60*60000);
-		r.lancer();
-		
-		System.out.println((System.currentTimeMillis() - millis) / 1000.0
-				+ " s");
-		System.out.println(r.getSolution());
-		System.out.println(r.getDistance());
+		Recherche r = new Recherche(nbg, tab_enorme, tabDesiderata, 60 * 6000);
+		r.execute();
+
+		System.out.println((System.currentTimeMillis() - millis) / 1000.0 + " s");
 	}
 }
